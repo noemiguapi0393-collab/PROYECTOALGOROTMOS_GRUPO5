5c8ce918337fae40a1d2a8a3d43e1f03805917d2
michaeliza2057-dotcom <michael.iza2057@utc.edu.ec>
2026-06-06 16:11:28 -0500

    implementando ciclo while

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -179,6 +179,8 @@ public class MenuPrincipal {
                     int numControl;
                     System.out.println("Ingrese un numero: ");
                     numControl =sn.nextInt();
+                    while(numControl >=0){
+                    }
                     break;
                     
                 }
