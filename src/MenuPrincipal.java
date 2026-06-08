09277b65e7e963cca2e614ad1fa6fee9b48e9cfe
noemiguapi0393-collab <noemi.guapi0393@utc.edu.ec>
2026-06-07 21:54:24 -0500

    FormTO DE SALIDA PROFESIONAL

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -327,11 +327,13 @@ public class MenuPrincipal {
                     }
                 }
                 case 14 -> {// Ejercicio 14: Suma de pares 1 al 200
-                System.out.println("\n--- SUMA DE PARES (1-200) ---");
-                // Cambio: Usamos una fórmula directa para evitar usar bucles con llaves que rompan el switch
+                System.out.println("\n=================================");
+                System.out.println("--- SUMA DE PARES (1-200) ---");
+                System.out.println("=================================");
                 System.out.println("La suma de los pares es: " + (100 * 101));
                 break;
                 }
+                
                 case 15 -> System.out.println("Saliendo del programa...");
                 default -> System.out.println("Opcion no valida.");
             }
