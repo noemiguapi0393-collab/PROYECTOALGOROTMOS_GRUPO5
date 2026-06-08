7ede088ead1cadec4a94cc909dcf9b76f53541be
noemiguapi0393-collab <noemi.guapi0393@utc.edu.ec>
2026-06-07 21:59:23 -0500

    Mensaje informativo del algoritmo usado

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -330,6 +330,7 @@ public class MenuPrincipal {
                 System.out.println("\n=================================");
                 System.out.println("--- SUMA DE PARES (1-200) ---");
                 System.out.println("=================================");
+                System.out.println("Optimizando proceso mediante la Formula de Gauss...");
                 long tiempoInicio = System.nanoTime();
                 int resultadoFormula = 100 * 101;
                 long tiempoFin = System.nanoTime();
