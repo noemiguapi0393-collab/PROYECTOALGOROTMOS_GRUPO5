0c0b799aa7d5aa478ef73e0b743cbedba2804252
noemiguapi0393-collab <noemi.guapi0393@utc.edu.ec>
2026-06-07 22:00:08 -0500

    Control de flujo

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -340,6 +340,7 @@ public class MenuPrincipal {
                     System.out.println("La suma de los pares es: " + resultadoFormula);
                 }
                 System.out.println("Tiempo de procesamiento: " + (tiempoFin - tiempoInicio) + " ns");
+                System.out.println("---------------------------------");
                 break;
                 }
                 
